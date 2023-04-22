1a9f4d9de2fea7a5be78cbd8f922ad0c8727df04
vsensu <vsensu@foxmail.com>
2023-04-22 18:55:37 +0800

    Fixing Shadow Acne

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
         return Color::new(0.0, 0.0, 0.0);
     }
 
-    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+    if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
         let target = rec.p + rec.normal + Vec3::random_in_unit_sphere();
         let r = Ray::new(rec.p, target - rec.p);
         0.5 * ray_color(&r, world, depth - 1)
