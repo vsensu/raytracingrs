8030d51fd28994c03475fb6def5e00a733fcc476
vsensu <vsensu@foxmail.com>
2023-04-23 12:12:59 +0800

    A Scene with Shiny Metal Spheres

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod camera;
 mod hit;
+mod material;
 mod ray;
 mod sphere;
 mod vec;
@@ -9,8 +10,13 @@ use hit::{Hit, World};
 use rand::Rng;
 use ray::Ray;
 use sphere::Sphere;
-use std::io::{stderr, Write};
+use std::{
+    io::{stderr, Write},
+    rc::Rc,
+};
 use vec::{Color, Point3, Vec3};
+
+use crate::material::{Lambertian, Metal};
 
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
@@ -20,9 +26,15 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
 
     if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
         // let target = rec.p + rec.normal + Vec3::random_in_unit_sphere().normalized();
-        let target = rec.p + Vec3::random_in_hemisphere(rec.normal);
-        let r = Ray::new(rec.p, target - rec.p);
-        0.5 * ray_color(&r, world, depth - 1)
+        // let target = rec.p + Vec3::random_in_hemisphere(rec.normal);
+        // let r = Ray::new(rec.p, target - rec.p);
+        // 0.5 * ray_color(&r, world, depth - 1)
+
+        if let Some((attenuation, scattered)) = rec.mat.scatter(r, &rec) {
+            attenuation * ray_color(&scattered, world, depth - 1)
+        } else {
+            Color::new(0.0, 0.0, 0.0)
+        }
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -35,13 +47,25 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u64 = 256;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
-    const SAMPLES_PER_PIXEL: u64 = 100;
-    const MAX_DEPTH: u64 = 5;
+    const SAMPLES_PER_PIXEL: u64 = 300;
+    const MAX_DEPTH: u64 = 50;
 
     // World
     let mut world = World::new();
-    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
+    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+
+    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
+    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
+    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
+    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
+
+    world.push(Box::new(sphere_ground));
+    world.push(Box::new(sphere_center));
+    world.push(Box::new(sphere_left));
+    world.push(Box::new(sphere_right));
 
     // Camera
     let cam = Camera::new();
--- src/vec.rs
+++ src/vec.rs
@@ -99,6 +99,15 @@ impl Vec3 {
             (-1.0) * in_unit_sphere
         }
     }
+
+    pub fn near_zero(self) -> bool {
+        const EPS: f64 = 1.0e-8;
+        self[0].abs() < EPS && self[1].abs() < EPS && self[2].abs() < EPS
+    }
+
+    pub fn reflect(self, n: Vec3) -> Vec3 {
+        self - 2.0 * self.dot(n) * n
+    }
 }
 
 impl Index<usize> for Vec3 {
@@ -175,6 +184,16 @@ impl Mul<Vec3> for f64 {
     fn mul(self, other: Vec3) -> Vec3 {
         Vec3 {
             e: [self * other[0], self * other[1], self * other[2]],
+        }
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self[0] * other[0], self[1] * other[1], self[2] * other[2]],
         }
     }
 }
