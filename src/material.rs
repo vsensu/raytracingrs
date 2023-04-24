9d3aeb5aba26f875455e01fb556b12925a0229b9
vsensu <vsensu@foxmail.com>
2023-04-24 18:21:18 +0800

    Fuzzy Reflection

--- src/material.rs
+++ src/material.rs
@@ -34,18 +34,19 @@ impl Scatter for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(a: Color) -> Metal {
-        Metal { albedo: a }
+    pub fn new(a: Color, f: f64) -> Metal {
+        Metal { albedo: a, fuzz: f }
     }
 }
 
 impl Scatter for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
         let reflected = r_in.direction().reflect(rec.normal).normalized();
-        let scattered = Ray::new(rec.p, reflected);
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
 
         if scattered.direction().dot(rec.normal) > 0.0 {
             Some((self.albedo, scattered))
